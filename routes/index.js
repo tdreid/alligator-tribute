859e4599031706fd1d0fa74b0592ce180dca3365
tdreid <trevor.d.reid@gmail.com>
2018-11-04 20:38:29 +0000

    Change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rjay | A slim experiment' });
+  res.render('index', { title: 'The American Alligator' });
 });
 
 module.exports = router;
